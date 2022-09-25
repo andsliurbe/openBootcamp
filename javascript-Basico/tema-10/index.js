ac402041ed4137f248a338df09fa7e4a18ab02ac
andsliurbe <andsliurbebogota@gmail.com>
2022-09-25 11:25:43 -0500

    Tema 10 Ejecricio 1

---
+++ javascript-Basico/tema-10/index.js
@@ -0,0 +1,6 @@
+import { suma,multiplica } from "./controller/controller.js";
+import chalk from 'chalk';
+
+
+console.log(`mostrando suma ->${suma(2,14)}`)
+console.log(chalk.blue(`mostrando multiplicacion ->${multiplica(2,14)}`))
